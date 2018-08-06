b9562f37954ac520c9921d42ddbcdff5e71a2767
aqsa <aqsajamil476@gmail.com>
2018-08-06 09:38:31 -0700

    math operation in python

---
+++ ex1.py
@@ -0,0 +1,41 @@
+#simple 1st program in python.......
+
+print("helo world")
+print("i like typing this")
+print("this is fun")
+
+
+
+
+
+
+#  Numbers and Math
+
+#+ plus
+
+#- minus
+
+#/ slash
+
+#* asterisk
+
+#% percent
+
+#< less- than
+
+#> greater- than
+
+#<= less- than- equal
+
+#>= greater- than- equal
+
+print("now i will count my chickens")
+print("hens",25 + 30 // 6)
+
+print(3 +  2 - 4 * 5 / 7 + 7 + 2 - 9)
+
+print(3 + 2 < 5 + 3)
+
+print(3 + 2 > 5 + 3)
+
+print ("What is 3 + 2?", 3 + 2)
